ad714f104d98541c39e4ac1180afec4575d25118
θεοχαρης παρισης <parisisxaris@gmail.com>
2024-06-01 18:47:51 +0300

    q

--- signup.php
+++ signup.php
@@ -22,7 +22,7 @@
 
          //verifying the unique email
 
-         $verify_query = mysqli_query($con,"SELECT email FROM users WHERE email='$email'");
+         $verify_query = mysqli_query($con,"SELECT Email FROM User WHERE Email='$email'");
 
          if(mysqli_num_rows($verify_query) !=0 ){
             echo "<div class='message'>
@@ -32,7 +32,7 @@
          }
          else{
 
-            mysqli_query($con,"INSERT INTO test(Username,Email,Name,Surname,Password) VALUES('$username','$email','$name','$surname','$password')") or die("Error Occured");
+            mysqli_query($con,"INSERT INTO User(Username,Email,Name,Surname,Password) VALUES('$username','$email','$name','$surname','$password')") or die("Error Occured");
 
             echo "<div class='message'>
                       <p>Registration successfully!</p>
@@ -50,19 +50,19 @@
         <h2>Sign Up</h2>
         <div class="form-group">
           <label for="name">Name</label>
-          <input type="text" id="name" name="name" required />
+          <input type="text" id="name" name="name" required autocomplete="off"/>
         </div>
         <div class="form-group">
           <label for="surname">Surname</label>
-          <input type="text" id="surname" name="surname" required />
+          <input type="text" id="surname" name="surname" required autocomplete="off"/>
         </div>
         <div class="form-group">
           <label for="username">Username</label>
-          <input type="text" id="username" name="username" required />
+          <input type="text" id="username" name="username" required autocomplete="off"/>
         </div>
         <div class="form-group">
           <label for="email">Email</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" id="email" name="email" required autocomplete="off"/>
         </div>
         <div class="form-group">
           <label for="password">Password</label>
@@ -78,10 +78,11 @@
             digit, one uppercase letter, and one special character.</small
           >
         </div>
-        <button type="submit">Sign Up</button>
+        <input type="submit" class="btn" name="submit" value="Register" required>
       </form>
-      <?php } ?>
+     
     </div>
+    <?php } ?>
     <script src="script.js"></script>
   </body>
 </html>
